60d585ef53600c947ada9277213db107dd200958
Cattimus <github_commit@cattimus.dev>
2024-02-27 14:43:32 -0800

    Changed function name to load_files since it can be used for templates and source files

--- web-gen.js
+++ web-gen.js
@@ -4,8 +4,8 @@ var build_dir = "";
 var src_dir = "";
 var template_dir = "";
 
-//assemble loaded file structure from template dir
-async function load_templates(path, templates) {
+//load files and their contents into a dictionary
+async function load_files(path, dictionary) {
 	let dir = await fs.opendir(path)
 	.catch((error) => {
 		console.log(`Unable to open directory: ${path}`)
@@ -14,22 +14,22 @@ async function load_templates(path, templates) {
 
 	//guard case for if directory doesn't exist
 	if (dir == null) {
-		return null;
+		return;
 	}
 
 	//read out filenames
 	for await(file of dir) {
 		if(file.isFile()) {
 			let name = file.path + "/" + file.name;
-			templates[name] = await fs.readFile(name);
+			dictionary[name] = await fs.readFile(name);
 		} else {
-			await load_templates(file.path +"/" + file.name, templates);
+			await load_files(file.path +"/" + file.name, dictionary);
 		}
 	}
 }
 
 let templates = {};
-load_templates("old", templates)
+load_files("old", templates)
 .catch((err) => {
 	console.log(`Something went wrong with loading templates: ${err}`);
 })

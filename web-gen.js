457b53d8ada1dbd137cf85b503201facac6e4056
Cattimus <github_commit@cattimus.dev>
2024-02-27 18:35:28 -0800

    Added function to handle command line args and helptext

--- web-gen.js
+++ web-gen.js
@@ -1,8 +1,9 @@
 const fs = require('node:fs/promises');
 
-var build_dir = "";
-var src_dir = "";
+var build_dir = "build";
+var src_dir = "src";
 var template_dir = "";
+var filetypes = [];
 
 //load files and their contents into a dictionary
 async function load_files(path, dictionary) {
@@ -28,14 +29,93 @@ async function load_files(path, dictionary) {
 	}
 }
 
-let templates = {};
-load_files("old", templates)
+function print_helptext() {
+	console.log();
+	console.log("[web-gen]:");
 
-.catch((err) => {
-	console.log(`Something went wrong with loading templates: ${err}`);
-	templates = null;
-})
+	console.log("-s,-source (path)");
+	console.log("Path to the folder that contains source files");
+	console.log();
 
-.finally(() => {
-	console.log(templates["old/Program.cs"]);
-})
+	console.log("-p,-parse (list,of,extensions)");
+	console.log("Tell web-gen which types of files you want to be run through the parser.");
+	console.log();
+
+	console.log("-t,-template (path)");
+	console.log("Path to the folder that contains templates.");
+	console.log();
+
+	console.log("-b,-build (path)");
+	console.log("Path to the output(build) directory.");
+	console.log();
+
+	console.log("[Directives]:");
+	console.log("Copy: <WGT>path_to_file.html</WGT>");
+	console.log("Copy a template into the source code.");
+	console.log();
+}
+
+//extract data from command line arguments
+function handle_args() {
+	for(let i = 2; i < process.argv.length; i++) {
+		switch(process.argv[i]) {
+			//template directory
+			case "-template":
+			case "-t":
+				template_dir = process.argv[i+1];
+				i++;
+			break;
+
+			//build directory
+			case "-build":
+			case "-b":
+				build_dir = process.argv[i+1];
+				i++;
+			break;
+
+			//source directory
+			case "-source":
+			case "-s":
+				src_dir = process.argv[i+1];
+				i++;
+			break;
+
+			//parse extensions
+			case "-parse":
+			case "-p":
+				filetypes = process.argv[i+1].split(",");
+				i++;
+			break;
+
+			case "--help":
+			case "-h":
+				print_helptext();
+				process.exit(0);
+			break;
+
+			//unrecognized argument
+			default:
+				console.log("Argument not recognized: '" + process.argv[i] + "'");
+				print_helptext();
+			break;
+		}
+	}
+}
+
+//print helptext if no args are given
+if(process.argv.length < 3) {
+	print_helptext();
+	process.exit(0);
+}
+
+//print error message if no template dir is given
+if(template_dir == "") {
+	console.log("You must at least specify a template directory to use web-gen.");
+	print_helptext();
+}
+
+handle_args();
+console.log(`build dir: ${build_dir}`);
+console.log(`source dir: ${src_dir}`);
+console.log(`template dir: ${template_dir}`);
+console.log(`filetypes: ${filetypes}`)
